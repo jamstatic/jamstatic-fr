1422f0392f818839f0675f76573b866aadc7e903
Frank Taillandier <frank.taillandier@gmail.com>
2018-07-12 22:10:35 +0200

    add Browserlist for autoprefixer

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    plugins: {
+        autoprefixer: {
+            browsers: [
+                "> 0.5% in my stats",
+                "> 5% in FR"
+            ]
+        }
+    },
+}
