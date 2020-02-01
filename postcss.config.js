56ac5a1e9545723bee400147d93298c62c8110d0
Frank Taillandier <frank.taillandier@gmail.com>
2020-02-01 11:30:47 +0100

    chore: optimize JS and CSS loading

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,11 @@
 module.exports = {
   plugins: {
+    "@fullhuman/postcss-purgecss": {
+      content: ["./layouts/**/*.html"],
+      whitelist: []
+    },
     autoprefixer: {
-      overrideBrowserslist: [
-        '> 0.5% in my stats',
-        '> 5% in FR'
-      ]
+      overrideBrowserslist: ["> 0.5% in my stats", "> 5% in FR"]
     }
   }
-}
+};
