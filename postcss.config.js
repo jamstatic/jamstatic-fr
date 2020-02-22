c14d219206559759566b68245b30e2b58b162b27
Frank Taillandier <frank.taillandier@gmail.com>
2020-02-22 20:39:29 +0100

    fix: purgecss whitelist

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@ module.exports = {
   plugins: {
     "@fullhuman/postcss-purgecss": {
       content: ["./layouts/**/*.html"],
-      whitelist: []
+      whitelist: ["highlight", "notice", "pre", "code", "chroma", "blockquote"]
     },
     autoprefixer: {
       overrideBrowserslist: ["> 0.5% in my stats", "> 5% in FR"]
