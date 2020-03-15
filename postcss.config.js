2a91206aee68c8be99bc259dc378ff349e7bb6fd
Frank Taillandier <frank.taillandier@gmail.com>
2020-03-16 00:44:21 +0100

    fix: whitelist fill-current for SVG icons

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@ module.exports = {
   plugins: {
     "@fullhuman/postcss-purgecss": {
       content: ["./layouts/**/*.html"],
-      whitelist: ["highlight", "notice", "pre", "code", "chroma", "blockquote"]
+      whitelist: ["highlight", "notice", "pre", "code", "chroma", "blockquote", "fill-current"]
     },
     autoprefixer: {
       overrideBrowserslist: ["> 0.5% in my stats", "> 5% in FR"]
