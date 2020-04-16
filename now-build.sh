2f95e737a3a8c6bb1475277e0cc7a130623cda3e
Arnaud Ligny <arnaud@ligny.org>
2020-04-16 22:28:11 +0200

    Update now-build.sh

--- now-build.sh
+++ now-build.sh
@@ -1,26 +1,17 @@
 #!/bin/bash
 
 # install Go
-echo "================================================================================"
-echo "Installing Go..."
 curl -sSOL https://dl.google.com/go/go1.13.linux-amd64.tar.gz
 tar -C /usr/local -xzf go1.13.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 
-echo "================================================================================"
-echo "Hugo current version:"
-hugo version
-
 # install Hugo
-echo "================================================================================"
 echo "Installing Hugo 0.69.0..."
 curl -sSOL https://github.com/gohugoio/hugo/releases/download/v0.69.0/hugo_0.69.0_Linux-64bit.tar.gz
 tar -xzf hugo_0.69.0_Linux-64bit.tar.gz
 ./hugo version
 
 # install theme(s)
-echo "================================================================================"
-echo "Installing theme(s)..."
 #git submodule update --init --recursive
 #git submodule add https://github.com/DirtyF/hugo-json-api-component themes/json-api
 curl -sSOL https://github.com/DirtyF/hugo-json-api-component/archive/master.zip
@@ -29,6 +20,5 @@ mkdir -p ./themes/json-api
 mv ./hugo-json-api-component-master/* ./themes/json-api/
 
 # run Hugo
-echo "================================================================================"
 echo "Building site..."
-./hugo --gc --minify
+./hugo
