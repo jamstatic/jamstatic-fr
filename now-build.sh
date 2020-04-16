16e4bcc756bd7ffe57a2c29b6023d4258be326a7
Arnaud Ligny <arnaud@ligny.org>
2020-04-16 21:41:08 +0200

    Update now-build.sh

--- now-build.sh
+++ now-build.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
 # install Go
+echo "Installing Go..."
 curl -sSOL https://dl.google.com/go/go1.13.linux-amd64.tar.gz
 tar -C /usr/local -xzf go1.13.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 
+echo "Hugo current version:"
 hugo version
 
 # install Hugo
-
+echo "Installing Hugo 0.69.0..."
 curl -sSOL https://github.com/gohugoio/hugo/releases/download/v0.69.0/hugo_0.69.0_Linux-64bit.tar.gz
 tar -xzf hugo_0.69.0_Linux-64bit.tar.gz
 
 ./hugo version
 
 # run Hugo
-
+echo "Building site..."
 ./hugo --gc --minify
