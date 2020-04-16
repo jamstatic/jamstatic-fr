d0ea174e19a1899a45edad8926206fdc189902c1
Arnaud Ligny <arnaud@ligny.org>
2020-04-16 22:43:50 +0200

    Update now-build.sh

--- now-build.sh
+++ now-build.sh
@@ -2,7 +2,7 @@
 
 # install Go
 curl -sSOL https://dl.google.com/go/go1.14.1.linux-amd64.tar.gz
-tar -C /usr/local -xzf go1.13.linux-amd64.tar.gz
+tar -C /usr/local -xzf go1.14.1.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 
 # install Hugo
