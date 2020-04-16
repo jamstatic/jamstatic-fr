7e87a11e305259b6fde7c9933aa0f2ebe98e327a
Arnaud Ligny <arnaud@ligny.org>
2020-04-16 21:38:41 +0200

    Update now-build.sh

--- now-build.sh
+++ now-build.sh
@@ -10,7 +10,7 @@ hugo version
 # install Hugo
 
 curl -sSOL https://github.com/gohugoio/hugo/releases/download/v0.69.0/hugo_0.69.0_Linux-64bit.tar.gz
-tar -xvzf hugo_0.69.0_Linux-64bit.tar.gz
+tar -xzf hugo_0.69.0_Linux-64bit.tar.gz
 
 ./hugo version
 
