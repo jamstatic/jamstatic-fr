1288dcdf29b1a92ddeb26a0b7268d71c8db4979e
Arnaud Ligny <arnaud@ligny.org>
2020-04-16 23:02:31 +0200

    Update now-build.sh

--- now-build.sh
+++ now-build.sh
@@ -2,16 +2,15 @@
 
 # install Go
 if [ -z $GO_VERSION ]; then GO_VERSION='1.14.1'; fi
-curl -sSOL https://dl.google.com/go/go$GO_VERSION.linux-amd64.tar.gz
-tar -C /usr/local -xzf go$GO_VERSION.linux-amd64.tar.gz
+curl -sSOL https://dl.google.com/go/go${GO_VERSION}.linux-amd64.tar.gz
+tar -C /usr/local -xzf go${GO_VERSION}.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 
 # install Hugo
 if [ -z $HUGO_VERSION ]; then HUGO_VERSION='0.69.0'; fi
 echo "Installing Hugo $HUGO_VERSION..."
-echo https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz
-curl -OL https://github.com/gohugoio/hugo/releases/download/v$HUGO_VERSION/hugo_$HUGO_VERSION_Linux-64bit.tar.gz
-tar -xzf hugo_$HUGO_VERSION_Linux-64bit.tar.gz
+curl -OL https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz
+tar -xzf hugo_${HUGO_VERSION}_Linux-64bit.tar.gz
 ./hugo version
 
 # install theme(s)
