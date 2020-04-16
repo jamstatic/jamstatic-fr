9d603528c8a6a4fe5c6fa3eea7b645fbcbabc2e4
Arnaud Ligny <arnaud@ligny.org>
2020-04-16 23:52:04 +0200

    Update now-build.sh Update now-build.sh Update now-build.sh Update now-build.sh

--- now-build.sh
+++ now-build.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-# install Go
+# default versions
 if [ -z $GO_VERSION ]; then GO_VERSION='1.14.1'; fi
+if [ -z $HUGO_VERSION ]; then HUGO_VERSION='0.69.0'; fi
+
+# install Go
 curl -sSOL https://dl.google.com/go/go${GO_VERSION}.linux-amd64.tar.gz
 tar -C /usr/local -xzf go${GO_VERSION}.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 
 # install Hugo
-if [ -z $HUGO_VERSION ]; then HUGO_VERSION='0.69.0'; fi
 echo "Installing Hugo $HUGO_VERSION..."
-curl -sSOL https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz
-tar -xzf hugo_${HUGO_VERSION}_Linux-64bit.tar.gz
+curl -sSOL https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz
+tar -xzf hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz
 ./hugo version
 
 # install theme(s)
-#git submodule update --init --recursive
-#git submodule add https://github.com/DirtyF/hugo-json-api-component themes/json-api
 curl -sSOL https://github.com/DirtyF/hugo-json-api-component/archive/master.zip
 unzip -q master.zip
 mkdir -p ./themes/json-api
