904003ca00e4d5544e3960750b9b086b55db4b1f
Arnaud Ligny <arnaud@ligny.org>
2020-04-16 21:25:40 +0200

    Update now-build.sh

--- now-build.sh
+++ now-build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-hugo version
+wget https://dl.google.com/go/go1.13.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.13.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+source ~/.bash_profile
 
+hugo version
 hugo --gc --minify
