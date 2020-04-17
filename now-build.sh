1a976f598a0fc9e7d80441d2fe94585efd6598cc
Arnaud Ligny <arnaud@ligny.org>
2020-04-18 01:23:00 +0200

    Update now-build.sh

--- now-build.sh
+++ now-build.sh
@@ -10,7 +10,7 @@ tar -C /usr/local -xzf go${GO_VERSION}.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 
 # install Node
-curl -sL https://install-node.now.sh | sh
+curl -sL https://install-node.now.sh | sh --yes
 
 # install Hugo
 echo "Installing Hugo $HUGO_VERSION..."
