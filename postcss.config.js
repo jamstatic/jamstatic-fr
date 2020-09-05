e4697e3c7df9a056416b2cde0745d6476f304d4e
Frank Taillandier <frank.taillandier@gmail.com>
2020-09-05 21:13:38 +0200

    fix: notices

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@ module.exports = {
   plugins: {
     "@fullhuman/postcss-purgecss": {
       content: ["./layouts/**/*.html"],
-      whitelist: ["highlight", "notice", "pre", "code", "chroma", "blockquote", "fill-current"]
+      whitelist: ["highlight", "notice", "info", "tip", "update", "pre", "code", "chroma", "blockquote", "fill-current"]
     },
     autoprefixer: {
       overrideBrowserslist: ["> 0.2% in my stats", "> 2% in FR"]
