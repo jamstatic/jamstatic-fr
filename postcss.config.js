a6da120caad2a68c0a3349a442d1923c66399d03
Arnaud Ligny <arnaud@ligny.org>
2022-10-23 23:55:14 +0200

    Cleaning

--- postcss.config.js
+++
@@ -1,7 +0,0 @@
-const tailwind = require('tailwindcss');
-const autoprefixer = require('autoprefixer');
-
-module.exports = {
-	// eslint-disable-next-line no-process-env
-	plugins: [ tailwind, ...(process.env.HUGO_ENVIRONMENT === 'production' ? [ autoprefixer ] : []) ]
-};
